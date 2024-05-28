2c252006f2a7120f1b944cff5af31706d6881436
Jurian Torres <jurilen@hotmail.com>
2024-05-28 12:03:31 -0600

    Proyectos agregrados

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,8 +11,6 @@ export class ProjectsComponent implements OnInit, AfterViewInit{
   ngOnInit(): void {
 
   }
-
-//Animacion de titulo utilizando IntersectionObserver para detectar cuando el titulo esta en el viewport
   ngAfterViewInit(): void {
 
     const observer = new IntersectionObserver((entries, observer) => {
@@ -32,14 +30,21 @@ export class ProjectsComponent implements OnInit, AfterViewInit{
   responsiveOptions: any[] = [];
 
 
-    product: any = {
-        image: 'product.jpg'
-    };
+
 
     constructor() {
-        for(let i = 0; i < 6; i++) {
-            this.projects.push(this.product);
-        }
+
+      this.projects = [
+        { id: '1',title: 'Tareas', imageUrl: 'assets/projects/tasks.png', linkUrl: 'https://pacific-spire-65839-b62b03a951e8.herokuapp.com/', description: 'Aplicación creada con react y backend desarrollado en Go ',githubUrl:'https://github.com/jurian-byte/tasksFront' },
+
+        { id: '2',title: 'Gestión bibliotecaria', imageUrl: 'assets/projects/sabe.png', linkUrl: 'https://drive.google.com/drive/folders/190xiGwZqElD8fk9p-E2Z1J2FbZ0RIgg4?usp=sharing', description: 'Aplicación creada con Angular',
+        githubUrl:'https://github.com/jurian-byte/SABEfront'},
+        { id: '3',title: 'App de clima',imageUrl: 'assets/projects/weather-app.png', linkUrl: 'https://young-wave-15612-ebd929ea1655.herokuapp.com/' , description: 'Aplicación creada con react consumiendo open-meteo',githubUrl:'https://github.com/jurian-byte/weather-app' },
+
+      ];
+
+
+
 
         this.responsiveOptions = [
             {
